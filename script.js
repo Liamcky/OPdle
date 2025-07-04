e04f9c9d96f97ef1c05ad0ad7f6011ee09137c72
Liamcky <horvce@gmail.com>
2025-07-04 23:02:17 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const Saga = Object.freeze({
+  "East Blue" = 0,
+  "Arabasta" = 1,
+  "Sky Island" = 2,
+  "Water 7" = 3,
+  "Thriller Bark" = 4,
+  "Summit War" = 5,
+  "Fish-Man Island" = 6,
+  "Dressrosa" = 7,
+  "Whole Cake Island" = 8,
+  "Wano Country" = 9,
+  "Final" = 10
+});
+const Arc = Object.freeze({
+  "Romance Dawn" = 0,
+  "Orange Town" = 1,
+  "Syrup Village" = 2,
+  "Baratie" = 3,
+  "Arlong Park" = 4,
+  "LogueTown" = 5
+});
