89958cfacaccf0b2bb779c0f2ecd5a51919903cc
Liamcky <horvce@gmail.com>
2025-07-22 23:42:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -435,7 +435,7 @@ const row = document.createElement("tr");
       svg.setAttribute("width", "1em");
       svg.setAttribute("height", "1em");
       svg.setAttribute("fill", "#f0f6fc");
-      svg.style.verticalAlign = "baseline";
+      svg.style.verticalAlign = "middle";
 
       const path = document.createElementNS(SVG_NS, "path");
       path.setAttribute("d", berrysvg);
